cdaa22010ef8754971cb74669c2248bf85f8c237
G A <gdur.mugen@googlemail.com>
2013-11-06 17:03:23 +0100

    - added LinearClippedMatrixFunction - added LinearUnclippedMatrixFunction 	- added successful JUnit tests for both
    - added all Sigmoid functions to the Menu

--- CBIRProjekt/RBM/src/de/htw/iconn/rbm/functions/tests/MatrixFunctionTests.java
+++ CBIRProjekt/RBM/src/de/htw/iconn/rbm/functions/tests/MatrixFunctionTests.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import de.htw.iconn.rbm.functions.DefaultLogisticMatrixFunction;
 import de.htw.iconn.rbm.functions.GaussMatrixFunction;
+import de.htw.iconn.rbm.functions.LinearClippedMatrixFunction;
+import de.htw.iconn.rbm.functions.LinearUnclippedMatrixFunction;
 import de.htw.iconn.rbm.functions.RectifierMatrixFunction;
 import de.htw.iconn.rbm.functions.TanHMatrixFunction;
 
@@ -38,21 +40,37 @@ public class MatrixFunctionTests {
 		assert2D(new double[][] { { .423 } }, rectifierMatrixFunction.function(matPosPointFive).toArray2(), delta);
 		assert2D(new double[][] { { 4.34 } }, rectifierMatrixFunction.function(matPosTen).toArray2(), delta);
 
-		// RectifierMatrixFunction
+		// TanHMatrixFunction
 		TanHMatrixFunction tanHMatrixFunction = new TanHMatrixFunction();
 		assert2D(new double[][] { { -1 } }, tanHMatrixFunction.function(matNegTen).toArray2(), delta);
 		assert2D(new double[][] { { -.462 } }, tanHMatrixFunction.function(matNegPointFive).toArray2(), delta);
 		assert2D(new double[][] { { 0 } }, tanHMatrixFunction.function(matZero).toArray2(), delta);
 		assert2D(new double[][] { { .462 } }, tanHMatrixFunction.function(matPosPointFive).toArray2(), delta);
 		assert2D(new double[][] { { 1} }, tanHMatrixFunction.function(matPosTen).toArray2(), delta);
-		
-		// RectifierMatrixFunction
+
+		// GaussMatrixFunction
 		GaussMatrixFunction gaussMatrixFunction = new GaussMatrixFunction();
 		assert2D(new double[][] { { 0 } }, gaussMatrixFunction.function(matNegTen).toArray2(), delta);
 		assert2D(new double[][] { { .778 } }, gaussMatrixFunction.function(matNegPointFive).toArray2(), delta);
 		assert2D(new double[][] { { 1 } }, gaussMatrixFunction.function(matZero).toArray2(), delta);
 		assert2D(new double[][] { { .778 } }, gaussMatrixFunction.function(matPosPointFive).toArray2(), delta);
 		assert2D(new double[][] { { 0} }, gaussMatrixFunction.function(matPosTen).toArray2(), delta);
+
+		// LinearClippedMatrixFunction
+		LinearClippedMatrixFunction linearClippedMatrixFunction = new LinearClippedMatrixFunction();
+		assert2D(new double[][] { { -1 } }, linearClippedMatrixFunction.function(matNegTen).toArray2(), delta);
+		assert2D(new double[][] { { -.5 } }, linearClippedMatrixFunction.function(matNegPointFive).toArray2(), delta);
+		assert2D(new double[][] { { 0 } }, linearClippedMatrixFunction.function(matZero).toArray2(), delta);
+		assert2D(new double[][] { { .5} }, linearClippedMatrixFunction.function(matPosPointFive).toArray2(), delta);
+		assert2D(new double[][] { { 1} }, linearClippedMatrixFunction.function(matPosTen).toArray2(), delta);
+
+		// LinearUnclippedMatrixFunction
+		LinearUnclippedMatrixFunction linearUnclippedMatrixFunction = new LinearUnclippedMatrixFunction();
+		assert2D(new double[][] { { -10 } }, linearUnclippedMatrixFunction.function(matNegTen).toArray2(), delta);
+		assert2D(new double[][] { { -.5 } }, linearUnclippedMatrixFunction.function(matNegPointFive).toArray2(), delta);
+		assert2D(new double[][] { { 0 } }, linearUnclippedMatrixFunction.function(matZero).toArray2(), delta);
+		assert2D(new double[][] { { .5} }, linearUnclippedMatrixFunction.function(matPosPointFive).toArray2(), delta);
+		assert2D(new double[][] { { 10} }, linearUnclippedMatrixFunction.function(matPosTen).toArray2(), delta);
 				
 	}
 
