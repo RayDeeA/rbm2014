ccc00764c31a26532b1b18d37d89f7397e84c3f3
Nexus' Child <mumucger@gmail.com>
2013-11-13 15:39:09 +0100

    Fixed LinearClippedFunction

--- CBIRProjekt/RBM/src/de/htw/iconn/rbm/functions/LinearClippedMatrixFunction.java
+++ CBIRProjekt/RBM/src/de/htw/iconn/rbm/functions/LinearClippedMatrixFunction.java
@@ -12,8 +12,8 @@ public class LinearClippedMatrixFunction extends MatrixFunctions implements
 		double[][] duplicateM = m.dup().mul(0.5).add(0.5).toArray2();
 		for (int y = 0; y < duplicateM.length; y++) {
 			for (int x = 0; x < duplicateM[y].length; x++) {
-				if (duplicateM[y][x] < -1)
-					duplicateM[y][x] = -1;
+				if (duplicateM[y][x] < 0)
+					duplicateM[y][x] = 0;
 				
 				if (duplicateM[y][x] > 1)
 					duplicateM[y][x] = 1;
