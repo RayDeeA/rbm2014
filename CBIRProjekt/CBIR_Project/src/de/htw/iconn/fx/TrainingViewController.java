8681d35fff07a8bda82fb659286c7268cc35c2a4
turbodiesel <stefan.err@gmail.com>
2013-12-05 15:34:39 +0100

    TrainingView changes

--- CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/TrainingViewController.java
+++ CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/TrainingViewController.java
@@ -8,21 +8,36 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
  *
- * @author dvarul
  */
 public class TrainingViewController implements Initializable, IFXController {
 
     private ArrayList<Double> errors = new ArrayList<Double>();
+
+    @FXML
+    private LineChart<Number, Number> chart_line_training;
+    
+    @FXML
+    private AnchorPane view;
     
     final int width = 500, height = 400, margin_top = 60, margin_right = 300, margin_bottom = 20, margin_left = 20;
 
+    public void addGraph() {
+    	
+    }
+    
+    public void addErrorToGraph(Double error) {
+    	errors.add(error);
+    }
     
     /**
      * Initializes the controller class.
@@ -34,6 +49,6 @@ public class TrainingViewController implements Initializable, IFXController {
 
     @Override
     public Node getView() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    	return view;
     }
 }
