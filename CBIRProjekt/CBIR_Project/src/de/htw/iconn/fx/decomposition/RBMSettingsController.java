b2094c7bb0baa403530932fc345644e6b8ea0043
Nexus' Child <mumucger@gmail.com>
2013-12-10 02:06:13 +0100

    Different subsettings in tree addable

--- CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/decomposition/RBMSettingsController.java
+++ CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/decomposition/RBMSettingsController.java
@@ -9,14 +9,25 @@ package de.htw.iconn.fx.decomposition;
 import de.htw.iconn.rbm.IRBM;
 import de.htw.iconn.rbm.RBMJBlas;
 import de.htw.iconn.rbm.functions.DefaultLogisticMatrixFunction;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
@@ -45,6 +56,10 @@ public class RBMSettingsController extends AController {
     
     private RBMSettingsModel model;
     
+    private AController[] controllers;
+    private TreeItem[] items;
+
+    
     
     
 
@@ -56,8 +71,41 @@ public class RBMSettingsController extends AController {
         //dummy rbm only temporary for testing
         IRBM rbm = new RBMJBlas(15 , 4, 0.1, new DefaultLogisticMatrixFunction());
         this.model = new RBMSettingsModel(rbm);
+        
         TreeItem settingsRBM = new TreeItem<String>("RBM");
+        settingsRBM.setExpanded(true);
         TreeItem settingsMain = new TreeItem<String>("Main");
+        RBMSettingsMainController controller = null;
+        try {
+            controller = (RBMSettingsMainController) loadController("RBMSettingsMain.fxml");
+           
+        } catch (IOException ex) {
+            Logger.getLogger(RBMSettingsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if(controller != null)
+        controllers = new AController[]{controller};
+        items = new TreeItem[]{settingsMain};
+        
+        trv_rbmSettingsMenue.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        
+        trv_rbmSettingsMenue.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>(){
+
+            @Override
+            public void changed(ObservableValue<? extends TreeItem<String>> ov, TreeItem<String> oldItem, TreeItem<String> newItem) { 
+                int idx = 0;
+                for (int i = 0; i < items.length; i++) {
+                    if(items[i] == newItem) {
+                        idx = i;
+                        break;
+                    }
+                }        
+                vbox_rbmSettingsTemplatePane.getChildren().clear();
+                vbox_rbmSettingsTemplatePane.getChildren().add(controllers[idx].getView());
+            }
+            
+        });
+        
         settingsRBM.getChildren().add(settingsMain);
         trv_rbmSettingsMenue.setRoot(settingsRBM);
         
