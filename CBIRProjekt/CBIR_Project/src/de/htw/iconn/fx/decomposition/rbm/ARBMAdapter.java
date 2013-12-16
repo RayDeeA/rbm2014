9970ee9875fe1099cac4e1cd90aca7b83ffa8874
Gnork <jansen.christoph@yahoo.de>
2013-12-16 21:48:28 +0100

    ARBMAdapter is now IRBM

--- CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/decomposition/rbm/ARBMAdapter.java
+++ CBIRProjekt/CBIR_Project/src/de/htw/iconn/fx/decomposition/rbm/ARBMAdapter.java
@@ -1,6 +1,6 @@
 package de.htw.iconn.fx.decomposition.rbm;
 
-public abstract class ARBMAdapter {
+public abstract class ARBMAdapter implements IRBM{
 
 	protected IRBM rbm;
 
@@ -8,22 +8,24 @@ public abstract class ARBMAdapter {
 		this.rbm = rbm;
 	}
 
+        @Override
 	public void train(double[][] data, int maxEpoche, boolean binarizeHidden, boolean binarizeVisible) {
 		rbm.train(data, maxEpoche, binarizeHidden, binarizeVisible);
 	}
 
+        @Override
 	public double error(double[][] data, boolean binarizeHidden, boolean binarizeVisible) {
 		return rbm.error(data, binarizeHidden, binarizeVisible);
 	}
 
+        @Override
 	public abstract double[][] getHidden(double[][] data, boolean binarizeHidden);
-    public abstract double[][] getVisible(double[][] data, boolean binarizeVisible);
+        
+        @Override
+        public abstract double[][] getVisible(double[][] data, boolean binarizeVisible);
 
+        @Override
 	public double[][] getWeights() {
 		return rbm.getWeights();
 	}
-
-    public IRBM getRBM() {
-        return rbm;
-    }
 }
